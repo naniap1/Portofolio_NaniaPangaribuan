a3130e8657fa29231830323c3069bfa802356347
naniap1 <124246838+naniap1@users.noreply.github.com>
2024-11-03 20:21:01 +0700

    Add files via upload

---
+++ Web Fortopolio_Nania Pangaribuan/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('twitter-icon').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Akun Twitter belum disambungkan.');
+});
+
+document.getElementById('instagram-icon').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Akun Instagram belum disambungkan.');
+});
